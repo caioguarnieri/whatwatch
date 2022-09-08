405d6ec7ce8cbe73afef3eb198fee4cb062ddd78
heliodcf <heliodcf@gmail.com>
2022-09-08 16:56:51 -0400

    Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,14 @@
-
+import "../components/home.css"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="body">
+        <div className="descricao">
+        <h2> VOCÊ TAMBÉM JÁ SE SENTIU PERDIDO QUANDO FOI ESCOLHER UM FILME!? </h2>
+        <h1> OU </h1>
+        <h2> SEM NENHUMA VONTA PARA PROCURAR UM BOM FILME!? </h2>
+        </div>
+    </div>
   )
 }
 
