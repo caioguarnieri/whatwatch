0cb4c329e9cfbae7e4a65eb6cf2afb627e2f6aee
caioguarnieri <caioguarniele@hotmail.com>
2022-09-09 00:16:07 -0400

    body div position fixed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,14 +3,14 @@ import "../components/home.css"
 const Home = () => {
   return (
     <div className="body">
-
+      
         <div className="descricao">
         
     
         <h1> Feeling lost and looking for  <br></br> a movie to watch? <br></br>We can Help!</h1>
         
         <button className="givemovie"> GIVE ME A MOVIE ! </button>
-       
+        
         </div>
     </div>
   )
