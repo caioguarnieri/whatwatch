4573b2b2df0a76f00fd02437d2e712b4ff9abd0d
caioguarnieri <caioguarniele@hotmail.com>
2022-09-23 12:49:18 -0400

    i think is finished

--- src/components/MovieFunction.js
+++ src/components/MovieFunction.js
@@ -1,12 +1,12 @@
-export function movieLength(movieRuntime){
-    let hours = Math.floor(movieRuntime / 60);
-    let minutes = movieRuntime % 60;
-    return hours + " h " + minutes + "min";
+export function movieLength(movieRuntime) {
+  let hours = Math.floor(movieRuntime / 60);
+  let minutes = movieRuntime % 60;
+  return hours + " h " + minutes + "min";
 }
 
 export function movieDirector(crew) {
-    const movieDirector = crew.find((item) => {
-        return item.job === "Director"
-    })
-    return movieDirector
+  const movieDirector = crew.find((item) => {
+    return item.job === "Director";
+  });
+  return movieDirector;
 }
